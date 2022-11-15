d1df8dac4f54abe7897627ff1327c538f8ea28e6
NathanrenardC2 <nathan.renard@etu.univ-grenoble-alpes.fr>
2022-11-15 12:55:09 +0100

    doc réalisé

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,7 +60,7 @@ const config = {
             type: 'doc',
             docId: 'intro',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Doc',
           },
         ],
       },
